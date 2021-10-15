17f31d92ab52f6dec20023b2a23c913fdf939611
Sazzad <netsazzad@gmail.com>
2021-10-15 13:47:57 +0600

    readme file added and testing implemented

--- src/redux/cart/reducer.test.ts
+++ src/redux/cart/reducer.test.ts
@@ -1,8 +1,22 @@
-import { cartReducer } from './reducer';
+import { CartActionTypes } from './types';
 import { initialState } from './reducer';
+import { cartReducer } from './reducer';
+
 describe('The cart reducer', () => {
   it('returns the input state for an unknown action', () => {
     const state = cartReducer(initialState, { type: undefined });
     expect(state).toEqual(initialState);
+  });
+
+  /*   it('should return the initial state', () => {
+    expect(cartReducer(undefined, {})).toMatchSnapshot();
+  }); */
+
+  it('should handle CART_REQUEST', () => {
+    expect(
+      cartReducer(initialState, {
+        type: CartActionTypes.GET_CART_SUCCESS,
+      })
+    ).toMatchSnapshot();
   });
 });
--- src/redux/product/reducer.test.ts
+++ src/redux/product/reducer.test.ts
@@ -1,9 +1,17 @@
 import { productReducer } from '../product/reducer';
 import { initialState } from '../product/reducer';
-import { ProductActionTypes } from '../product/types';
+import { ProductActionTypes } from './types';
 describe('The product reducer', () => {
   it('returns the input state for an unknown action', () => {
     const state = productReducer(initialState, { type: undefined });
     expect(state).toEqual(initialState);
+  });
+
+  it('should handle CART_REQUEST', () => {
+    expect(
+      productReducer(initialState, {
+        type: ProductActionTypes.GET_PRODUCT_SUCCESS,
+      })
+    ).toMatchSnapshot();
   });
 });
